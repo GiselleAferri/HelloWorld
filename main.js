fa1569ffe8578463c1cd3843b1dfce3635b29100
GiselleAferri <168047072+GiselleAferri@users.noreply.github.com>
2024-04-25 09:54:03 -0300

    Construção do main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+let nomeUsuásio = "";
+let elemento = document.querySelector("#nomeUsuário");
+
+while(nomeUsuário == ""){
+    nomeUsuásio = prompt("Qual o seu nome?");
+}
+
+if(nomeUsuário == null){
+    elemento.textContent = "Seja muito bem-vindo";
+}else{
+    elemento.textContent = nomeUsuário;
+}
